7f29a0bcd63e30e50fddb473756c5eb127e62fdd
masnada lilian <lilian.masnada@gmail.com>
2025-04-23 20:12:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+    "manifests": {
+        "acme/private-bundle": {
+            "manifest": {
+                "bundles": {
+                    "Acme\\PrivateBundle\\AcmePrivateBundle": [
+                        "all"
+                    ]
+                }
+            },
+            "ref": "7405f3af1312d1f9121afed4dddef636c6c7ff00"
+        }
+    }
+}
+
